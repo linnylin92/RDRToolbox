e1f72701eb122e9d06454ec61d1567e2862288d5
klin1234@gmail.com <klin1234@gmail.com>
2018-04-12 15:42:51 -0400

    Modifying the package for convenience

--- R/Isomap.R
+++ R/Isomap.R
@@ -3,18 +3,18 @@
 ## This version uses Floyd's Algorithm to compute the shortest path tree.
 ##
 ## A modified version of the original Isomap algorithm is included. It respects nearest and farthest neighbours.
-## To estimate the intrinsic dimension of the data, the function can plot the residuals between the high and the 
+## To estimate the intrinsic dimension of the data, the function can plot the residuals between the high and the
 ## low dimensional data for a given range of dimensions.
 ##
-## input: 
+## input:
 ##	data: 		NxD matrix (N samples, D features)
 ##	dims:		dimension of the target space (default 2)
 ##	k:		number of neighbours (default 5 or number of samples)
 ##	mod:		use modified Isomap algorithm (default FALSE)
 ##	plotResiduals:	show a plot with the residuals between the high and the low dimensional data (default FALSE)
-##	verbose:	show a summary of the embedding procedure at the end (default TRUE)	 
+##	verbose:	show a summary of the embedding procedure at the end (default TRUE)
 ##
-## output: 
+## output:
 ##	Nxdim matrix (N samples, dim features) with the reduced input data (list of several matrices if more than one dimension specified)
 
 
@@ -39,7 +39,7 @@ Isomap = function(data, dims=2, k, mod=FALSE, plotResiduals=FALSE, verbose=TRUE)
 
         k = round(k)
         dims = round(dims)
-        
+
         num_samples = nrow(data)
         num_features = ncol(data)
 
@@ -78,13 +78,13 @@ Isomap = function(data, dims=2, k, mod=FALSE, plotResiduals=FALSE, verbose=TRUE)
 		d_row_i = d_row_i[rep(1, each = num_samples),]
 		d = pmin(d, d_col_i + d_row_i)
 	}
-        
+
 	## determine all connected components of the graph
 	num_connections = rowSums(!(is.infinite(d)))
 	first_connections = apply(is.infinite(d),1,which.min)
 	components = unique(first_connections)
 	num_components = length(components)
-	message("done")	
+	message("done")
 	## reduce dimension of all components seperately and merge them to a single dataset
 	message("Computing low dimensional embedding ... ", appendLF = FALSE)
 	all_Y = list(NULL)
@@ -93,37 +93,30 @@ Isomap = function(data, dims=2, k, mod=FALSE, plotResiduals=FALSE, verbose=TRUE)
 	for(dim in dims){
 		Y = matrix(0,num_samples,dim)
 		for(c in 1:num_components){
-	
+
 			## only use the distances of points in the connected component
 			comp_indices = which(first_connections == components[c])
 			D = d[comp_indices,comp_indices]
 			N = length(comp_indices)
 
-			## convert graph matrix to inner products	
-			T = -0.5 * ( D^2 - rowSums(D^2) %*% t(rep(1/N,N)) - rep(1,N) %*% t(rowSums(D^2))/N + sum(D^2)/(N^2))			
+			## convert graph matrix to inner products
+			T = -0.5 * ( D^2 - rowSums(D^2) %*% t(rep(1/N,N)) - rep(1,N) %*% t(rowSums(D^2))/N + sum(D^2)/(N^2))
 
                         ## catch possible errors caused by too small connected components
                         if(dim(T)[1] < dim)
                           stop("Problem occured while computing embedding: Connected component ", c, " consists of too few samples (", dim(T)[1], ") and cannot be reduced to ", dim, " dimensions. Try Isomap with a neighbourhood parameter higher than ", k, " or with dimension parameters lower than ", dim, ".")
-                        
+
 			## embedding Y is given by the eigenvectors of T belonging to (some of) its biggest eigenvalues
 	   		eig_T = eigen(T)
 			sweep(eig_T$vectors, 2, sqrt(colSums(eig_T$vectors^2)), "/")	#normalize eingenvectors
-               		Y[comp_indices,] = Re(eig_T$vectors[,1:dim] * (matrix(1,N,1) %*%  sqrt(as.complex(eig_T$values[1:dim]))))		
+               		Y[comp_indices,] = Re(eig_T$vectors[,1:dim] * (matrix(1,N,1) %*%  sqrt(as.complex(eig_T$values[1:dim]))))
 		}
-		## calculate residual variances (take care of not neighboured samples (Inf values))
-		if(plotResiduals == TRUE){
-                        tmp = as.vector(d)
-                        tmp[is.infinite(tmp)] = NA
-			r = 1 - cor(cbind(tmp ,as.vector(pairwiseDistances(Y))), use="complete.obs")^2
-			residuals[i] = r[1,2]
-                        rm(tmp)
-		}
+
 		all_Y[[i]] = Y
 		i = i+1
 	}
 	message("done")
-	names(all_Y) = paste("dim", dims, sep="")	
+	names(all_Y) = paste("dim", dims, sep="")
 
 	## give a summary of what has been done
 	if(verbose == TRUE){
@@ -131,10 +124,6 @@ Isomap = function(data, dims=2, k, mod=FALSE, plotResiduals=FALSE, verbose=TRUE)
 		message("reduction from ", num_features, " to ", dims, " dimensions")
 		message("number of connected components in graph: ", num_components)
 	}
-	
-	## plot residual variances for all dimensions
-	if(plotResiduals == TRUE)
-		plot(dims,residuals,type="b",xlab="dimension",ylab="residual variance")
 
 	return(all_Y)
 
